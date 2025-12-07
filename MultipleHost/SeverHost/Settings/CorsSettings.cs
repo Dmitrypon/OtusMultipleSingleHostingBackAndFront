3b0c13564ebddf0e4955978c6bc7703960c2ad93
Dmitry Ponomar <mitrybox@gmail.com>
2025-12-07 19:48:39 +0300

    CorsSettings added

---
+++ MultipleHost/SeverHost/Settings/CorsSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace SeverHost.Settings
+{
+    public class CorsSettings
+    {
+        public string[] Origins { get; set; }
+        public string[] Headers { get; set; }
+        public string[] Methods { get; set; }
+    }
+}
